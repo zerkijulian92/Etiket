ed3454e3ff5090baf671c913dce5fe3090482ae2
zerkijulian92 <zerkijuliandri92@gmail.com>
2019-05-21 16:34:21 +0700

    Setting timer di SplashAct.Java

--- app/src/main/java/com/takatutustudio/etiket/SplashAct.java
+++ app/src/main/java/com/takatutustudio/etiket/SplashAct.java
@@ -1,5 +1,7 @@
 package com.takatutustudio.etiket;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,17 @@ public class SplashAct extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //setting timer untuk 1 detik
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //berpindah ke Activity Getstarted
+                Intent gogetstarted = new Intent(SplashAct.this, GetStartedAct.class);
+                startActivity(gogetstarted);
+                finish();
+            }
+        }, 1000); // 1000 ms = 1s
     }
 }
