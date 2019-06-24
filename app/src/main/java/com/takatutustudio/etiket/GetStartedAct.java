7b06ad56718018cac8c31abfed5a8aeccf4eb9bc
zerkijulian92 <zerkijuliandri92@gmail.com>
2019-06-24 18:20:39 +0700

    Menyelesaikan tampilan Sign TiketSaya

--- app/src/main/java/com/takatutustudio/etiket/GetStartedAct.java
+++ app/src/main/java/com/takatutustudio/etiket/GetStartedAct.java
@@ -1,13 +1,28 @@
 package com.takatutustudio.etiket;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GetStartedAct extends AppCompatActivity {
+
+    Button btn_sign_in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
+
+        btn_sign_in = findViewById(R.id.btn_sign_in);
+
+        btn_sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotosign = new Intent(GetStartedAct.this, SigninAct.class);
+                startActivity(gotosign);
+            }
+        });
     }
 }
