73c3be940fea3aa364e33fa38066a20e640644e2
zerkijulian92 <zerkijuliandri92@gmail.com>
2019-06-24 15:23:30 +0700

    Membuat Activity SignInAct

---
+++ app/src/main/java/com/takatutustudio/etiket/SigninAct.java
@@ -0,0 +1,13 @@
+package com.takatutustudio.etiket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SigninAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
